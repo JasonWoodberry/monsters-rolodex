cdbeb79525e3337f15046181e02ed3728aa45243
Jason Woodberry <jwoodberry@quintevents.com>
2020-05-24 23:03:57 -0400

    Updates

---
+++ precache-manifest.beb6b7c243b6ea6e83c589082dc65045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f9c1b64bb354b2797030e4c436dbec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd1a51a8b01d96e82fef",
+    "url": "/monsters-rolodex/static/css/main.047b51f5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1a51a8b01d96e82fef",
+    "url": "/monsters-rolodex/static/js/main.3ea63a5d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
